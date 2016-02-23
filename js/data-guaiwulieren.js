5406aa38cf67fed13448f8fe2301c6f9dd6dc76a
lucasters <xiaoli880105@163.com>
2016-02-23 16:49:11 +0800

    打印武器分类页面

--- js/data-guaiwulieren.js
+++ js/data-guaiwulieren.js
@@ -24,27 +24,72 @@
 	
 	Guaiwulieren.prototype = {
 
+
+		getWeaponData:function(weapon_id){
+			var that = this,
+			weapon_data='';
+			for(var i=0;i<that.data_weapon.length;i++){
+				var temp = that.data_weapon[i].result;
+				for(var j=0;j<temp.length;j++)
+				if(weapon_id == temp[j].iID){					
+					return temp[j].data;
+				}				
+			}				
+		},
+
 		printDetail:function(){//打印详情页	
-			
+			var that = this;
 			var index = this.getsession("wbgl-guaiwulieren-weapon-type");
 			var html_title = '',
 				html_content = '',
+				html_small_icon = '',//是否可合成小图标
+				weapon_icon='',//武器icon
 				weapon_title_data = '',
-				weapom_content_data = '';				
+				weapom_content_data = '',
+				single_weapon_data= '',
+				tindex='',
+				tmep_c1='',
+				$weapon_head = $("#weapon_head"),
+				$weapon_content = $("#weapon_content");				
 			for(var i=0;i<this.data_layout[index].data.length;i++){				
 				weapon_title_data = this.data_layout[index].data[i];
-				html_title +='<div class="item on">'+weapon_title_data.gname+'</div>';	
-				if(i==3){//移动の城寨
+				html_title +='<div class="item">'+weapon_title_data.gname+'</div>';	
+				// if(i==3){//移动の城寨
 					for(var j=0;j<weapon_title_data.gdate.length;j++){				
-							weapom_content_data = weapon_title_data.gdate[j];
-							console.log(weapom_content_data.id);
-							html_content += '<div class="oldiv left'+weapom_content_data.line+' top'+weapom_content_data.row+'"><i class="icon_chuizi"></i><img class="weapon_icon" src="images/guaiwulieren/icon/ep_nb4_13.png"></div>'
+							weapom_content_data = weapon_title_data.gdate[j];	
+							single_weapon_data = that.getWeaponData(weapom_content_data.id);							
+							if(single_weapon_data[90] != ''){
+								html_small_icon = '<i class="icon_chuizi"></i>';
+							}	
+							if(single_weapon_data[4] != ''){
+								weapon_icon = '<img class="weapon_icon" src="images/guaiwulieren/icon/'+single_weapon_data[4]+'.png">';
+							}
+							html_content += '<div data_id="'+weapom_content_data.id+'" class="oldiv left'+weapom_content_data.line+' top'+weapom_content_data.row+'">'+html_small_icon+weapon_icon+'</div>';					
+							html_small_icon = '';
 						}
-				}
+						tmep_c1 += '<div class="content_item">'+html_content+'</div>';
+						html_content='';
+				// }
 			}
-			$("#herolist_head").html(html_title);
-			$("#weapon_content").html(html_content);
+			$weapon_head.html(html_title);
+			$weapon_content.html(tmep_c1);		
+			$weapon_head.on("click", ".item", function(){				
+				tindex = $weapon_head.children().index(this);				
+				that.setsession("wbgl-guaiwulieren-weapon-title-tindex",tindex);
+				$weapon_head.children().eq(tindex).addClass("on").siblings().removeClass("on");
+				$weapon_content.children().eq(tindex).addClass("on").siblings().removeClass("on");
+			}).children().eq(tindex).trigger("click");
 
+			$weapon_content.on("click", ".oldiv", function(){
+				var _id = Number($(this).attr("data_id"));
+				that.setsession("wbgl-guaiwulieren-weapon-id",_id);
+				console.log(_id);
+				// if(that.o.platform == "ios"){
+				// 	location.href = that.o.plugin+'/data-quanminchaoshen-hero-detail.html';
+				// }else{
+				// 	location.href = 'data-quanminchaoshen-hero-detail.html';
+				// }
+			});
 		},
 
 		printindex:function(){//打印Index,武器类型
@@ -66,6 +111,8 @@
 					location.href = 'data-guaiwulieren-weapon-detail.html';
 				}
 			});
+
+
 		},
 
 		checkversion: function(){//检查版本
@@ -100,7 +147,12 @@
 					}
 				break;
 				case "index-detail":
-
+					if(this.o.platform == "web"){
+							removehide();
+							$("#header").children(".back").attr("href","data-guaiwulieren-weapon.html");
+						}else if(this.o.platform == "android"){							
+							$("#header").children(".back").attr("href","data-guaiwulieren-weapon.html");							
+						}					
 				break;
 			}
 		},
@@ -108,14 +160,14 @@
 		ispage: function(){//判断当前打开的是哪一个页面
 			if(!this.checkversion()) return;
 
-			var href = $("body").attr("data-url");
-			console.log(href);
+			var href = $("body").attr("data-url");			
 			switch(true){
-				case (href == "index"):
+				case (href == "index.html"):
 					this.isplatform("index");
 					this.printindex();
 					break;
-				case (href == "index-detail"):
+				case (href == "index-detail.html"):
+					this.isplatform("index-detail");
 					this.printDetail();
 					break;
 			}
@@ -182,8 +234,29 @@
 			}
 		},
 
+		events: function(){
+			var that = this;
+			//旋转屏幕重新设置
+			$(window).resize(function(e){
+                that.pageReset();
+            });
+		},
+		pageReset: function(){//重置页面
+			this.setfontSize();			
+		},
+
+		setfontSize: function(){//根据宽度算整体字体大小
+			var doc = document,
+			docEle = doc.documentElement,
+			docEleW = docEle.clientWidth,
+			fs = docEleW/320*100;
+			docEle.style.fontSize = fs+"px";
+		},
+
 		init: function(){
 			this.ispage();
+			this.events();
+			this.pageReset();
 		}
 	}	
 	window.Guaiwulieren = Guaiwulieren;
