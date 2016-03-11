7044d211d2f95266369db8e3e101481897535ff6
lucasters <xiaoli880105@163.com>
2016-03-11 17:39:43 +0800

    打印皇室战争第一个页面 修改css

--- js/data-huangshizhanzheng.js
+++ js/data-huangshizhanzheng.js
@@ -2,7 +2,11 @@
 	var Huangshizhanzheng = function(option){
 		if(arguments[0] === undefined) return false;
 		this.data = typeof(arguments[0]) == 'object' ? arguments[0] : {};
-		this.o = {platform:"web",url:"images/huangshizhanzheng/"};
+		this.o = {
+					platform:"web",
+					url:"images/huangshizhanzheng/",
+					plugin:""
+				 };
 		if(this.o.platform == "android"){
 			this.o.url="../images/huangshizhanzheng/";
 		}
@@ -25,6 +29,128 @@
 			}
 			return true;
 		},
+
+		printIndex:function(){
+
+			var piece,
+				html_head = '',
+				html_content = '',
+				html_content_temp = '',
+				html_content_all = '';
+			for(var i=0; i<this.data.length; i++){
+				html_head += '<div class="item">'+this.data[i].typ+'</div>';
+				html_content += '<ul>';
+				html_content_temp='';
+				for(var j=0; j<this.data[i].data.length; j++){
+					piece = this.data[i].data[j];
+					html_content_temp += '<li data-type="'+(i+1)+'"+data-id="'+piece.id+'"><img src="'+this.o.url+'dbpic/'+piece.id+'.png"><p>'+piece.name+'</p></li>';
+				}
+				html_content += html_content_temp;
+				html_content_all += html_content_temp;
+				html_content += '</ul>';
+			}
+			$("#weapon_head").html(html_head);
+			$("#weapon_content").html(html_content);
+		},
+
+		printDetail:function(){
+
+		},
+
+		printCompare:function(){
+
+		},
+
+		events:function(){
+			var that = this,
+			tindex = 0;
+			$weapon_head = $("#weapon_head"),
+			$weapon_content = $("#weapon_content"),
+
+			/*兵种列表点击事件*/
+			$weapon_head.on("click", ".item", function(){
+				tindex = $weapon_head.children().index(this);
+				that.setsession("wbgl-huangshizhanzhanzheng-weapon-tindex",tindex);
+				$weapon_head.children().eq(tindex).addClass("on").siblings().removeClass("on");
+				$weapon_content.children().eq(tindex).addClass("on").siblings().removeClass("on");
+			}).children().eq(tindex).trigger("click");
+
+
+			$weapon_content.on("click", "li", function(){
+				var _id = Number($(this).attr("data-id"));
+					// _weapontype = Number($(this).attr("data-type"));
+				
+				that.setsession("wbgl-huangshizhanzheng-weapon-id",_id);
+				// that.setsession("wbgl-huangshizhanzheng-weapon-type",_weapontype);
+				if(that.o.platform == "ios"){
+					location.href = that.o.plugin+'/data-huangshizhanzheng-weapon-detail.html';
+				}else{
+					location.href = 'data-huangshizhanzheng-weapon-detail.html';
+				}
+			});
+			/*兵种详情点击事件*/
+			/*卡牌对比点击事件*/
+		},
+
+		getsession: function(){
+			var sessionname = arguments[0];
+			if(this.o.platform == "ios"){
+				return this.cookie(sessionname);
+			}else{
+				return sessionStorage.getItem(sessionname);
+			}
+		},
+		setsession: function(){
+			var sessionname = arguments[0],
+			sessionvalue = arguments[1];
+			if(this.o.platform == "ios"){
+				this.cookie(sessionname,sessionvalue);
+			}else{
+				sessionStorage.setItem(sessionname, sessionvalue);
+			}
+		},
+		cookie: function(name, value, options){
+			if(typeof value != 'undefined'){// name and value given, set cookie
+				options = options || {};
+				if(value === null){
+					value = '';
+					options.expires = -1;
+				}
+				var expires = '';
+				if(options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+					var date;
+					if(typeof options.expires == 'number') {
+						date = new Date();
+						date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+					}else{
+						date = options.expires;
+					}
+					expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+				}
+				// CAUTION: Needed to parenthesize options.path and options.domain
+				// in the following expressions, otherwise they evaluate to undefined
+				// in the packed version for some reason...
+				var path = options.path ? '; path=' + (options.path) : '';
+				var domain = options.domain ? '; domain=' + (options.domain) : '';
+				var secure = options.secure ? '; secure' : '';
+				document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+			}else{ // only name given, get cookie
+				var cookieValue = null;
+				if(document.cookie && document.cookie != ''){
+					var cookies = document.cookie.split(';');
+					for(var i = 0; i < cookies.length; i++){
+						var cookie = jQuery.trim(cookies[i]);
+						// Does this cookie string begin with the name we want?
+						if(cookie.substring(0, name.length + 1) == (name + '=')){
+							cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+							break;
+						}
+					}
+				}
+				return cookieValue;
+			}
+		},
+
 		isplatform: function(i){//判断打包平台显示相应内容
 			function removehide(){
 				$("#header").removeClass("hide");
@@ -41,6 +167,10 @@
 						$("#container").addClass("mt_0");
 					}
 				break;
+				case "detail":
+				break;
+				case "compare":
+				break;
 			}
 		},
 		ispage: function(){//判断当前打开的是哪一个页面
@@ -50,12 +180,17 @@
 			switch(true){
 				case (href == "index"):
 					this.isplatform("index");
-					//this.printindex();
+					this.printIndex();
+					break;
+				case (href == "detail"):
+					break; 
+				case (heef == "compare"):
 					break;
 			}
 		},
 		init: function(){
 			this.ispage();
+			this.events();
 		}
 	}
 	
